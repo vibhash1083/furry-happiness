bc4a2781580a44267d193b6101eb34012734628f
Srinivas Reddy Danda <srinivas.d@SLS.ads.valuelabs.net>
2019-12-23 14:27:29 +0530

    Added constants

--- src/Utils.js
+++ src/Utils.js
@@ -1,27 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import { API_ROOT } from 'constants/urls.js';
+import axios from 'axios';
 
-// The hook is just a simple function which we can export
 const useFetchBooks = () => {
 
-  // First we define the necessary states for our hook
-  // this includes book, the loading state and potential errors
+
   const [books, setBooks] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-
-  // useEffect can be compared to componentDidMount,
-  // componentDidUpdate and componentDidUnmount
-  // read more about useEffect here:
-  // https://reactjs.org/docs/hooks-effect.html
+  
   useEffect(() => {
 
     // First we set the loading and error states
     setLoading(true)
     setError(null)
-
-    fetch('http://localhost:8090/api/books')
+    console.log(API_ROOT+`books`)
+    axios.get(API_ROOT+'books')
       .then(res => res.json())
       .then(json => {
+        console.log(json[0].bookName);
         setLoading(false)
         if (json.books) {
             console.log("Hello")
---
+++ src/constants/urls.js
@@ -0,0 +1,2 @@
+export const BASE_URL ='http://localhost:8090/';
+export const API_ROOT = BASE_URL + 'api/';
